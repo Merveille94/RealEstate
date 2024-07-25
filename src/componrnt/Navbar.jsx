a96cc2fb13f5d457d1a656c1a7dacfc791f544aa
Merveille94 <emanueleguehi@gmail.com>
2024-07-25 07:00:48 +0000

    update

--- src/componrnt/Navbar.jsx
+++ src/componrnt/Navbar.jsx
@@ -2,17 +2,46 @@ import logo from "../assets/images/logo.png"
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>AbiSarb</div>
-        <ul>
-            <li>Home</li>
-            <li>Agents</li>
-            <li>Property</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+    <header className="p-4">
+      <div className="container mx-auto px-4 ">
+        <div className="flex items-center justify-between">
+          <div className="pr-4 pl-4">
+            <div className="text-xl font-bold">
+              <a href="index.html" className="text-gray-800 hover:text-gray-600">AbiSarb</a>
+            </div>
+          </div>
+          <div className=" text-right">
+            <span className="md:hidden inline-block py-5">
+              <a href="#" className="text-gray-800">
+                <span className="icon-menu h3 text-gray-800"></span>
+              </a>
+            </span>
+            <nav className="hidden md:block ">
+              <ul className="flex space-x-4">
+                <li className="active">
+                  <a href="index.html" className="nav-link text-gray-800 hover:text-gray-600">Home</a>
+                </li>
+                <li>
+                  <a href="agents.html" className="nav-link text-gray-800 hover:text-gray-600">Agents</a>
+                </li>
+                <li>
+                  <a href="property.html" className="nav-link text-gray-800 hover:text-gray-600">Property</a>
+                </li>
+                <li>
+                  <a href="about.html" className="nav-link text-gray-800 hover:text-gray-600">About</a>
+                </li>
+                <li>
+                  <a href="blog.html" className="nav-link text-gray-800 hover:text-gray-600">Blog</a>
+                </li>
+                <li>
+                  <a href="contact.html" className="nav-link text-gray-800 hover:text-gray-600">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
