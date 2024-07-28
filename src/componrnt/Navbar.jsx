d53e13aba884cf20a2432b53798605df3f8e0f6a
Merveille94 <emanueleguehi@gmail.com>
2024-07-28 19:37:11 +0000

    update

--- src/componrnt/Navbar.jsx
+++ src/componrnt/Navbar.jsx
@@ -1,47 +1,11 @@
-import logo from "../assets/images/logo.png"
+import React from 'react'
+import Logo_1 from '../assets/Logo/logo.png'
 
 const Navbar = () => {
   return (
-    <header className="p-4">
-      <div className="md:container md:mx-auto px-4 ">
-        <div className="flex items-center justify-between">
-          <div className="pr-4 pl-4">
-            <div className="text-xl font-bold">
-              <a href="index.html" className="text-gray-800 hover:text-gray-600">AbiSarb</a>
-            </div>
-          </div>
-          <div className=" text-right">
-            <span className="md:hidden inline-block py-5">
-              <a href="#" className="text-gray-800">
-                <span className="icon-menu h3 text-gray-800"></span>
-              </a>
-            </span>
-            <nav className="hidden md:block ">
-              <ul className="flex space-x-4">
-                <li className="active">
-                  <a href="index.html" className="nav-link text-gray-800 hover:text-gray-600">Home</a>
-                </li>
-                <li>
-                  <a href="agents.html" className="nav-link text-gray-800 hover:text-gray-600">Agents</a>
-                </li>
-                <li>
-                  <a href="property.html" className="nav-link text-gray-800 hover:text-gray-600">Properties</a>
-                </li>
-                <li>
-                  <a href="about.html" className="nav-link text-gray-800 hover:text-gray-600">About</a>
-                </li>
-                <li>
-                  <a href="blog.html" className="nav-link text-gray-800 hover:text-gray-600">Blogs</a>
-                </li>
-                <li>
-                  <a href="contact.html" className="nav-link text-gray-800 hover:text-gray-600">Contact</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
+    <div className='Navbar'>
+      <img src={Logo_1} alt="logo" className='w-20'/>
+    </div>
   )
 }
 
