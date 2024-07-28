2af340af0ac0bdd3a4e0ebd8c144d0f3340ccdc8
Merveille94 <emanueleguehi@gmail.com>
2024-07-28 20:29:47 +0000

    Update

--- src/componrnt/Navbar.jsx
+++ src/componrnt/Navbar.jsx
@@ -3,9 +3,20 @@ import Logo_1 from '../assets/Logo/logo.png'
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>
+    <nav className='md:container md:mx-auto px-7 py-2 '>
+
       <img src={Logo_1} alt="logo" className='w-20'/>
-    </div>
+
+      <ul>
+        <li><link to="">Home</link></li>
+        <li><link to="">Agents</link></li>
+        <li><link to="">Property</link></li>
+        <li><link to="">About</link></li>
+        <li><link to="">Blog</link></li>
+        <li><link to="">Contact</link></li>
+      </ul>
+
+    </nav>
   )
 }
 
