657e7df742badaf075b37c99de608cb1e2d03cae
Merveille94 <emanueleguehi@gmail.com>
2024-07-29 05:01:05 +0000

    Update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,15 +5,24 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-600">
       <div className="md:container md:mx-auto max-w-7xl px-2 sm:px:6 lg:px-8">
-        <div className="flex h-20 items-center justify-between bg-black">
-          <div className="flex flex-1 items-center md:items-stretch md:justify-start bg-white">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center md:items-stretch md:justify-start">
             <a
               href="/index.html"
               className="flex flex-shrink-0 items-center mr-4"
             >
-              <img src={Logo_1} alt="logo" className="h-10 w-auto" />
-              <span className="hidden md:block text-white text-sxl font-bold ml-2">ABISARB - RealEstate</span>
+              <img src={Logo_1} alt="logo" className="h-9 w-auto" />
             </a>
+            <div className="md:ml-auto">
+                <ul className="hidden md:flex space-x-2">
+                  <li><a href="/index.html" className="text-white hover:text-lime-300 px-3 py-2">Home</a></li>
+                  <li><a href="" className="text-white hover:text-lime-300 px-3 py-2">Agents</a></li>
+                  <li><a href="" className="text-white hover:text-lime-300 px-3 py-2">Property</a></li>
+                  <li><a href="" className="text-white hover:text-lime-300 px-3 py-2">About</a></li>
+                  <li><a href="" className="text-white hover:text-lime-300 px-3 py-2">Blog</a></li>
+                  <li><a href="" className="text-white hover:text-lime-300 px-3 py-2">Contact</a></li>
+                </ul>
+            </div>
           </div>
         </div>
       </div>
